d1004335e9724e53e7501b99afb0e4f5daadc85d
Sith <dreadsith@github.com>
2018-04-16 12:11:19 +0300

    Almost done.
    Gotta add a game over screen, maybe a main menu, will see.

---
+++ Assets/Additional Stuff/Scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour {
+
+    public float health = 30f;
+
+    public AnimationClip Anim;
+    private Animation myAnimation;
+    
+
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.isStopped = true;
+            GetComponent<Enemy>().enabled = false;
+            myAnimation = GetComponent<Animation>();
+            myAnimation.Play(Anim.name);
+            Invoke("Die", Anim.length);
+        }
+
+    }
+
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
